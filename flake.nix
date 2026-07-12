dcc1636548767ccd930fa32e09f791a0dbd8c46f
UzakiMorstar <uzakilunax@proton.me>
2026-07-12 21:38:34 +0800

    remove useless lines from flake.nix

--- flake.nix
+++ flake.nix
@@ -1,16 +1,14 @@
 {
-  description = "NixOS configuration with Niri and DMS";
+  description = "NixOS configuration for MorStar";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-    # 引入 DankMaterialShell 的稳定版 flake
     dms = {
       url = "github:AvengeMedia/DankMaterialShell/stable";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    # 引入 Niri 官方 flake (推荐，以获取最新特性)
     niri = {
       url = "github:niri-wm/niri";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -24,7 +22,6 @@
   outputs = { self, nixpkgs, dms, niri, ... }@inputs: {
     nixosConfigurations."nixos" = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
-      # 将 inputs 传递给其他 Nix 文件
       specialArgs = { inherit inputs; };
       modules = [
         ./configuration.nix

45aca920f0df9acda81611d5a5231fee21f5b638
UzakiMorstar <uzakilunax@proton.me>
2026-07-18 13:09:30 +0800

    update bitwarden-ssh-agent.nix

--- bitwarden-ssh-agent.nix
+++ bitwarden-ssh-agent.nix
@@ -1,19 +1,8 @@
-{ config, lib, ... }:
-
+{ config, ... }:
 {
-  home.activation = {
-    linkBitwardenSsh = config.lib.dag.entryAfter ["writeBoundary"] ''
-      USER_UID=$(id -u)
-      TARGET_DIR="/run/user/$USER_UID/gcr"
-      TARGET_SOCK="$TARGET_DIR/ssh"
-      
-      mkdir -p "$TARGET_DIR"
-      
-      if [ -e "$TARGET_SOCK" ] || [ -L "$TARGET_SOCK" ]; then
-        rm -f "$TARGET_SOCK"
-      fi
-      
-      ln -s "${config.home.homeDirectory}/.bitwarden-ssh-agent.sock" "$TARGET_SOCK"
-    '';
-  };
+  home.file.".config/fish/conf.d/bitwarden-ssh.fish".text = ''
+    set -gx SSH_AUTH_SOCK "${config.home.homeDirectory}/.bitwarden-ssh-agent.sock"
+  '';
+
+  services.ssh-agent.enable = false;
 }

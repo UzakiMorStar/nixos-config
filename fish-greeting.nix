1c03854c5e8d9b6b2f5a0d7e6e3afa13ac0acf73
UzakiMorstar <uzakilunax@proton.me>
2026-07-19 20:24:53 +0800

    add fish-greeting.nix and update home.nix

--- fish-greeting.nix
+++ fish-greeting.nix
@@ -1,6 +1,15 @@
 { config, ... }:
 {
   home.file.".config/fish/conf.d/greeting.fish".text = ''
-    set -g fish_greeting "MorStar Here! Definitely NOT femboy, nya~~~"
+    set -l trans_blue (set_color 55cdfc)
+    set -l trans_pink (set_color f7a8b8)
+    set -l trans_white (set_color ffffff)
+    set -l reset (set_color normal)
+
+    set -g fish_greeting "$trans_blue""Nya~~ Nya~~
+    $trans_pink""MørStãr here~~
+    $trans_white""I am DEFINITELY NOT femboy btw
+    $trans_pink""Coding, Gaming, dressing up today~~
+    $trans_blue""Still Cis Tho$reset"
   '';
 }

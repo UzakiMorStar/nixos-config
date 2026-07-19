62ccbb8035adaba15121d3b2206fde7a3359d997
UzakiMorstar <uzakilunax@proton.me>
2026-07-19 19:49:38 +0800

    update git-config.nix

--- git-config.nix
+++ git-config.nix
@@ -4,7 +4,7 @@
   programs.git = {
     enable = true;
     signing = {
-      key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAtct7KhFGEfKWFH8Sup3g1bW6VZv7iz2EPrhRW3ZMT8";
+      key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBnjVbeqbfW+vIkvCKtn8h7QehFKaqvbyToxVsA5o92Z";
       signByDefault = true;
     };
     settings = {

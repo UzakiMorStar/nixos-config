a627c77064e9f4a4ea0c51a48a232e977d0e4723
UzakiMorstar <uzakilunax@proton.me>
2026-07-20 10:24:29 +0800

    update home.nix

--- home.nix
+++ home.nix
@@ -32,6 +32,7 @@
     inputs.sidra.packages.${pkgs.stdenv.hostPlatform.system}.default
     stockfish
     en-croissant
+    claude-code-bin
     (pkgs.writeShellApplication {
        name = "kzzi-light";
        runtimeInputs = [ (pkgs.python3.withPackages (ps: [ ps.hidapi ])) ];

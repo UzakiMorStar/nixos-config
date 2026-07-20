491845f6423d0237294e865d3f223a1d47ccca25
UzakiMorstar <uzakilunax@proton.me>
2026-07-20 09:44:20 +0800

    update fish-greeting.nix

--- fish-greeting.nix
+++ fish-greeting.nix
@@ -1,15 +1,39 @@
 { config, ... }:
 {
   home.file.".config/fish/conf.d/greeting.fish".text = ''
-    set -l trans_blue (set_color 55cdfc)
-    set -l trans_pink (set_color f7a8b8)
-    set -l trans_white (set_color ffffff)
-    set -l reset (set_color normal)
+    function fish_greeting
+        set -l trans_blue (set_color 55cdfc)
+        set -l trans_pink (set_color f7a8b8)
+        set -l trans_white (set_color ffffff)
+        set -l reset (set_color normal)
 
-    set -g fish_greeting "$trans_blue""Nya~~ Nya~~
-    $trans_pink""MørStãr here~~
-    $trans_white""I am DEFINITELY NOT femboy btw
-    $trans_pink""Coding, Gaming, dressing up today~~
-    $trans_blue""Still Cis Tho$reset"
+        set -l lines \
+            "Nya~~ Nya~~" \
+            "MørStãr here~~" \
+            "I am DEFINITELY NOT femboy btw" \
+            "Coding, Gaming, dressing up today~~" \
+            "Still Cis Tho"
+
+        set -l colors \
+            $trans_blue \
+            $trans_pink \
+            $trans_white \
+            $trans_pink \
+            $trans_blue
+
+        set -l cols (tput cols 2>/dev/null; or echo 80)
+
+        for i in (seq (count $lines))
+            set -l text $lines[$i]
+            set -l color $colors[$i]
+            set -l visible_len (string length -- $text)
+            set -l pad (math "floor(($cols - $visible_len) / 2)")
+
+            if test $pad -gt 0
+                echo -n (string repeat -n $pad ' ')
+            end
+            echo $color$text$reset
+        end
+    end
   '';
 }
